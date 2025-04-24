24d825fcfdc61e993ea0fd209a80aef3737c0dab
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-24 16:23:22 -0400

    Portfolio-Website

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+
+
+
+
+
+function App() {
+
+  const handleClick = (label) => {
+    alert(`You clicked: ${label}`);
+  };
+
+
+
+
+  return (
+    <>
+      <img src="/bookshelf.PNG" className="sideImage rightSideImage" alt="right" />
+      <img src="/desk.PNG" className="sideImage leftSideImage" alt="left" />
+      <svg
+      className="sideSvg"
+      viewBox="0 0 1920 1080"
+      preserveAspectRatio="xMidYMid meet">
+
+         {/* Project-1 */}
+        <rect
+        x="1140"
+        y="460"
+        width="80"
+        height="220"
+        fill="transparent"
+        stroke="red"
+        strokeWidth="2"
+        onClick={() => handleClick("Region 1")}
+        style={{ cursor: 'pointer' }}
+        />
+
+        {/* Project-2 */}
+        <rect
+        x="1225"
+        y="520"
+        width="60"
+        height="160"
+        fill="transparent"
+        stroke="blue"
+        strokeWidth="2"
+        onClick={() => handleClick("Region 2")}
+        style={{ cursor: 'pointer' }}
+        />
+
+        {/* Project-3 */}
+        <rect
+        x="1330"
+        y="520"
+        width="40"
+        height="150"
+        fill="transparent"
+        stroke="green"
+        strokeWidth="2"
+        transform="rotate(-35 1350 600)" // rotate 45 degrees around the rect's center
+        onClick={() => handleClick("Region 3")}
+        style={{ cursor: 'pointer' }}
+        />
+  
+
+        {/* Resume */}
+        <rect
+        x="1710"
+        y="280"
+        width="160"
+        height="200"
+        fill="transparent"
+        stroke="red"
+        strokeWidth="2"
+        
+        onClick={() => window.open('/Liam Morris Resume.pdf', '_blank')}
+        style={{ cursor: 'pointer' }}
+        />
+      </svg>      
+    </>
+
+  );
+}
+
+export default App
