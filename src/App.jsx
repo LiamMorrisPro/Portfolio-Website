37ce596560e53dd36b625e6b045c489e0aac356f
LiamMorrisPro <LiamMorrisPro@gmail.com>
2025-04-24 20:43:25 -0400

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,92 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Hover() {
+  const [hovered, setHovered] = React.useState(true);
+  return (
+    <>
+      <img
+        src="ResumeHover.PNG"
+        alt="icon Johnson"
+          style={{
+            position: 'absolute',
+            height: '23vh',
+            top: '24vh',
+            right: '1vh',          
+            zIndex: 10,
+            opacity: hovered ? 1 : 0,
+            pointerEvents: 'none',
+          }}
+      />
+      <svg
+      className="sideSvg"
+      viewBox="0 0 1920 1080"
+      preserveAspectRatio="xMidYMid meet">
+      
+      <rect
+      x="1710"
+      y="280"
+      width="160"
+      height="200"
+      fill="rgba(255, 0, 0, 0.3)"
+      stroke="red"
+      strokeWidth="2"
+      
+      onClick={() => window.open('Liam Morris Resume.pdf', '_blank')}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      style={{position: 'absolute', cursor: 'pointer', zIndex: 11 }}
+      />
+    </svg> 
 
+    </>
 
+  );
+}
 
+function HoverImage({imagePath},{linkPath}){
+  const [hovered, setHovered] = useState(false);
+  return(
+  <>
+    <img
+        src="ResumeHover.PNG"
+        style={{
+          position: 'absolute',
+          height: '23vh',
+          top: '24vh',
+          right: 0,          
+          opacity: hovered ? 1 : 0,
+          transition: 'opacity 0.3s ease',
+          pointerEvents: 'none',
+          zIndex: 10,
+
+        }}
+    />
+    <svg
+      className="sideSvg"
+      viewBox="0 0 1920 1080"
+      preserveAspectRatio="xMidYMid meet">
+      {/* Resume */}
+      <rect
+      x="1710"
+      y="280"
+      width="160"
+      height="200"
+      fill="transparent"
+      stroke="red"
+      strokeWidth="2"
+      
+      onClick={() => window.open('Liam Morris Resume.pdf', '_blank')}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      style={{ cursor: 'pointer' }}
+      />
+    </svg>       
+  </>
+  );
+}
 
 
 
@@ -16,13 +97,15 @@ function App() {
     alert(`You clicked: ${label}`);
   };
 
-
-
-
   return (
     <>
       <img src="bookshelf.PNG" className="sideImage rightSideImage" alt="right" />
       <img src="desk.PNG" className="sideImage leftSideImage" alt="left" />
+
+      {/* Hover Buttons */}
+        {/* Resume */}
+        <Hover/>
+
       <svg
       className="sideSvg"
       viewBox="0 0 1920 1080"
@@ -67,24 +150,24 @@ function App() {
         onClick={() => handleClick("Region 3")}
         style={{ cursor: 'pointer' }}
         />
-  
 
-        {/* Resume */}
-        <rect
-        x="1710"
-        y="280"
-        width="160"
-        height="200"
+        {/* Links */}
+        <circle
+        cx="1320"
+        cy="150"
+        r="130"
         fill="transparent"
-        stroke="red"
+        stroke="blue"
         strokeWidth="2"
-        
-        onClick={() => window.open('Liam Morris Resume.pdf', '_blank')}
+        onClick={() => handleClick("Region 3")}
+        onMouseEnter={() => console.log("Mouse Entered")}
+        onMouseLeave={() => console.log("Mouse Exited")}
         style={{ cursor: 'pointer' }}
         />
-      </svg>      
-    </>
+      </svg>  
 
+  
+    </>
   );
 }
 
